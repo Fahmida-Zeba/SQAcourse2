28ee1b9651555b945d4729b42af73aaefc09c1ed
Fahmida-Zeba <fahmidahaquezeba@gmail.com>
2026-01-10 20:41:14 +0600

    first commit

---
+++ src/Javaclass1/Identifiers.java
@@ -0,0 +1,14 @@
+package Javaclass1;
+
+public class Identifiers {
+    public static void main(String[] args) {
+        int identifier = 10; // Rule 1
+        //String 9name; // invalid identifier due to having number in the first
+        //String conatin space; // invalid identifier for containing space
+        String csensitive;
+        String CSensitive; // case sensitive
+        //String int; // reserved keyword cannot be used
+
+
+    }
+}
